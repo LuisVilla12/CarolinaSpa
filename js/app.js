1f72bcbe22d7bc0c114f69d9682ce92483d48666
Luis Villa <luisji2312ia@gmail.com>
2021-12-24 14:06:09 -0600

    v14 Libreria Estilo 2

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
-// Crear un nuevo objeto
-window.swiper = new Swiper({
-    // Padre de los elementos
-    el: '.slider__contenedor',
-    // Elementos
-    slideClass: 'slider__slide',
-    createElements: true,
-    // Iniciar automaticamente
-    autoplay: {
-        // cambiar cada 3s
-        delay: 3000
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
     },
-    // Ciclo cuando acaba volver a iniciar
-    loop: true,
-    // Espacio entre las imagenes
-    spaceBetween: 30,
-    // Muesta la ubicacion
     pagination: {
-        el: '.slider__paginacion',
+        el: ".slider__paginacion",
     },
-    // Flechitas
-    navigation: true
+    loop: true
 });
