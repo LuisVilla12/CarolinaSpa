fded30e2e590864facfaa0ae20d275f30e1a62a9
Luis Villa <luisji2312ia@gmail.com>
2021-12-24 09:53:16 -0600

    v11 Slider Libreria

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Crear un nuevo objeto
+window.swiper = new Swiper({
+    // Padre de los elementos
+    el: '.slider__contenedor',
+    // Elementos
+    slideClass: 'slider__slide',
+    createElements: true
+});
